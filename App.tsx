91d15d73464fb18dd72c145718ef44b9799f4e5a
asmraihan <asmraihanctg14@gmail.com>
2023-07-16 17:06:18 +0600

    search

--- App.tsx
+++ App.tsx
@@ -1,12 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View, SafeAreaView } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import { Text, View, SafeAreaView, ScrollView } from 'react-native';
 import SearchBox from './components/SearchBox';
 
 export default function App() {
+
+  const search = (searchTerm: string) => {
+    if (searchTerm == "") {
+      alert("Please enter a username")
+      return
+    }
+    fetch(`https://api.github.com/users/${searchTerm}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+      });
+  }
   return (
-    <SafeAreaView className='bg-black flex-1'>
-      <SearchBox></SearchBox>
-     
+    <SafeAreaView className='bg-slate-900 flex-1'>
+      <ScrollView keyboardShouldPersistTaps="handled" className='h-screen'>
+        <SearchBox onSearch={search}></SearchBox>
+        {/* <StatusBar style="auto" /> */}
+      </ScrollView>
     </SafeAreaView>
   );
 }
--- components/SearchBox.tsx
+++ components/SearchBox.tsx
@@ -2,19 +2,19 @@ import { View,Text, TextInput, Pressable } from 'react-native'
 import React, { useState } from 'react'
 
 type Props = {
-
+    onSearch: Function
 }
 
-const SearchBox = (props: Props) => {
+const SearchBox = ({onSearch}: Props) => {
     const [userName , setUserName] = useState<string>('')
   return (
-    <View className='flex flex-row items-center space-x-4 mx-auto mt-5'>
-   <TextInput placeholder="Enter your username here... "
+    <View className='flex flex-row items-center space-x-4 mx-auto mt-10'>
+   <TextInput placeholder="Enter username ... "
       onChangeText={(text: string) => setUserName(text)} 
       className="border p-2 w-2/3 border-gray-100 rounded text-gray-100 focus:border-2 focus:border-blue-500 "
       placeholderTextColor="#ffffff" />
-       <Pressable className='bg-sky-600 px-4 py-2 rounded'>
-        <Text>Search</Text>
+       <Pressable className='bg-sky-600 px-4 py-3 rounded' onPress={()=>onSearch(userName)}>
+        <Text className='text-gray-100 font-semibold'>Search</Text>
        </Pressable>
 
     </View>
